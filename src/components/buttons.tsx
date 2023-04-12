779441ba1bb732bc6ab9afbc7f1a63b12561a125
ZacharyCallahan <zachcallahan90@gmail.com>
2023-04-12 12:00:00 +0000

    test

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -605,6 +605,7 @@ export const OptionsButton = ({ children, ...rest }: OptionsButtonProps) => {
             )}
             
 
+            
             {open && (
                 <div className="z-[999] absolute bg-slate-300 p-3 rounded-md flex flex-col items-start">
                     {children}
